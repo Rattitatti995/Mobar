49210a6c1c233453dd0a31a5d335fc8b4a73e72b
Rattitatti995 <skjevik.leif95@gmail.com>
2026-08-18 13:38:31 +0200

    Load complete MoBar product styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import './auth.css'
 import './v2.css'
 import './features.css'
+import './product.css'
 import AuthGate from '@/components/AuthGate'
 
 export const metadata: Metadata = {
